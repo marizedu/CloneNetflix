699212d666e4c5300910efc8a749502b94ac8fbc
marizedu <edumariz1103@gmail.com>
2021-03-25 20:39:43 -0300

    header concluido

---
+++ js/owl/setup.js
@@ -0,0 +1,25 @@
+$('.owl-carousel').owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: false,
+  responsive: {
+    0: {
+      items: 2,
+    },
+    480: {
+      items: 4,
+    },
+    640: {
+      items: 5,
+    },
+    720: {
+      items: 6,
+    },
+    1000: {
+      items: 8,
+    },
+    1250: {
+      items: 10,
+    },
+  },
+});
